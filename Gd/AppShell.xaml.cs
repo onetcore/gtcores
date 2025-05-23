1da69ea030fcef46f96650c7ca75c30dada87416
网魂小兵 <30725375@qq.com>
2025-05-23 11:21:40 +0800

    添加Maui项目。

---
+++ Gd/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Gd;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Gd/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Gd;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
