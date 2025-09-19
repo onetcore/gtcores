dc5df4ba7e8e20fbfd6d57067b93d6c92f948661
网魂小兵 <30725375@qq.com>
2025-09-19 22:34:53 +0800

    添加菜单。

---
+++ GtCores.Components/Navigations/IMenuProviderFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace GtCores.Components.Navigations;
+
+/// <summary>
+/// 菜单工厂类。
+/// </summary>
+public interface IMenuProviderFactory : ISingletonService
+{
+    /// <summary>
+    /// 加载当前名称的菜单列表。
+    /// </summary>
+    /// <param name="name">菜单提供者名称。</param>
+    /// <returns>返回菜单实例对象。</returns>
+    MenuItem? Load(string? name = null);
+}
